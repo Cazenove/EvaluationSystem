d47b03d087b28d3c80e9f7b411ffe9195b4a2663
xwh <60315356+xwh130@users.noreply.github.com>
2020-04-28 21:02:02 +0800

    Update UserRepository.java

--- backend/src/main/java/com/evaluation/system/dao/UserRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/UserRepository.java
@@ -2,10 +2,18 @@ package com.evaluation.system.dao;
 
 import com.evaluation.system.bean.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.persistence.Column;
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,String> {
     //通过组号查询
     public List<User> findByGroupId(int group_id);
+    //通过学号查询
+    public User findByUserId(String user_id);
+    //通过手机号查询
+    public  User findByUserIdAndTelephone(String telephone,String user_id);
+
 }
