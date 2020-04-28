eff570e1b2b4948b4589574df25a4dc773f7589a
lqi59780808 <41891928+lqi59780808@users.noreply.github.com>
2020-04-28 18:02:31 +0800

    班级接口更新

--- backend/src/main/java/com/evaluation/system/controller/ClassController.java
+++ backend/src/main/java/com/evaluation/system/controller/ClassController.java
@@ -1,22 +1,53 @@
 package com.evaluation.system.controller;
 
-import com.evaluation.system.ClassRepository;
+import com.evaluation.system.dao.ClassRepository;
 import com.evaluation.system.bean.Class;
+import com.evaluation.system.service.ClassService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
 
 //一个例子
 @RestController
 public class ClassController {
     @Autowired
+    private ClassService classService;
+
+    @Autowired
     private ClassRepository classRepository;
 
-    //列出所有管理员
-    @RequestMapping(value = "/admin/class/create")
-    public List<Class> listAdmin(){
-        return classRepository.findAll();
+    @PostMapping(value = "/admin/class/create")
+    public Map<String, Object> creatClass(@RequestBody Class classInfo){
+        Map<String, Object> result = classService.saveClass(classInfo);
+        return result;
     }
+
+    @PostMapping(value = "/admin/class/delete")
+    public Map<String, Object> deleteClass(@RequestBody Class classInfo){
+        Map<String, Object> result = classService.deleteClass(classInfo);
+        return result;
+    }
+
+    @PostMapping(value = "/admin/class/update")
+    public Map<String, Object> updateClass(@RequestBody Class classInfo){
+        Map<String, Object> result = classService.saveClass(classInfo);
+        return result;
+    }
+
+    @PostMapping(value = "/admin/class")
+    public Map<String, Object> findAllClass(){
+        Map<String, Object> result = classService.findAllClass();
+        return result;
+    }
+
+    @PostMapping(value = "/admin/class/end")
+    public Map<String, Object> endClass(@RequestBody Class classInfo){
+        Map<String, Object> result = classService.endClass(classInfo);
+        return result;
+    }
+
 }
--- backend/src/main/java/com/evaluation/system/dao/ClassRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/ClassRepository.java
@@ -1,10 +1,12 @@
-package com.evaluation.system;
+package com.evaluation.system.dao;
 
 import com.evaluation.system.bean.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,12 +15,7 @@ public interface ClassRepository extends JpaRepository<Class,Integer> {
     /*
     班级ID查询
      */
-    @Query("select c from Class as c where c.class_id = :class_id")
-    Class findByClassId(@Param("class_id") Integer class_id);
+    @Query("select c from Class as c where c.classId = :classId")
+    Class findByClassId(@Param("classId") Integer classId);
 
-    /*
-    查找全部
-     */
-    @Query("select c from Class as c")
-    List<Class> findAll();
 }
