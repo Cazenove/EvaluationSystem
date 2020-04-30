f487215e87333e977676c1d79fa520cc7e9c0f12
lqi59780808 <41891928+lqi59780808@users.noreply.github.com>
2020-04-30 21:49:55 +0800

    添加maven依赖，更新评分表实体

--- backend/src/main/java/com/evaluation/system/bean/EvaluationInner.java
+++ backend/src/main/java/com/evaluation/system/bean/EvaluationInner.java
@@ -1,14 +1,19 @@
 package com.evaluation.system.bean;
 
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Map;
 
 //组内评价表类
 
 @Entity
+@TypeDef(name = "json",typeClass = JsonStringType.class)
 public class EvaluationInner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,8 +21,10 @@ public class EvaluationInner {
     private String name;
     @Transient
     private int classId;
-    private String content;//这个暂时先用string
-    private Date releaseTime;//用的是util的date，不知道和sql的date有什么区别。出了问题再修改
+    @Type(type = "json")
+    private Map<String,Object> content;
+    private Date releaseTime;
+    private Date endTime;
 
     @ManyToOne
     @JoinColumn(name = "class_id")
@@ -28,6 +35,13 @@ public class EvaluationInner {
 
     }
 
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 
     public Class getClassInfo() {
         return classInfo;
@@ -61,11 +75,11 @@ public class EvaluationInner {
         this.classId = classId;
     }
 
-    public String getContent() {
+    public Map<String, Object> getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(Map<String, Object> content) {
         this.content = content;
     }
 
