96a2d176de7602556309a6e670aed64fdb53e337
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-01 20:44:49 +0800

    Delete GroupEvaluation.java

--- backend/src/main/java/com/evaluation/system/dao/GroupEvaluation.java
+++
@@ -1,9 +0,0 @@
-package com.evaluation.system.dao;
-
-import com.evaluation.system.bean.Evaluation_outer;
-import com.evaluation.system.bean.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupEvaluation extends JpaRepository<Evaluation_outer,Integer> {
-    public Evaluation_outer findByEvaluationOuterId(int id);
-}
