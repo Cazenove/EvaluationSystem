e267b3fda040b6fad0e6f4e1546cd96a6b11ffb1
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-01 20:44:27 +0800

    Update GroupEvaluationService.java

--- backend/src/main/java/com/evaluation/system/service/GroupEvaluationService.java
+++ backend/src/main/java/com/evaluation/system/service/GroupEvaluationService.java
@@ -1,31 +1,78 @@
 package com.evaluation.system.service;
 
-import com.evaluation.system.bean.Evaluation_outer;
-import com.evaluation.system.dao.Group_Evaluation;
+import com.evaluation.system.bean.EvaluationOuter;
+import com.evaluation.system.dao.GroupEvaluationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
+/**
+ * @author 221701109_肖玮昊
+ */
 @Service
 public class GroupEvaluationService {
     @Autowired
-    private Group_Evaluation group_evaluation;
+    private GroupEvaluationRepository groupEvaluation;
+
+    //按班级号显示组间评价列表
+    public  HashMap<String,Object> listEvaluation(EvaluationOuter evaluationOuter){
+        int classId = evaluationOuter.getClassId();
+        List<EvaluationOuter> evaluationOuters = groupEvaluation.findAllByClassId(classId);
+        HashMap<String,Object> result = new HashMap<>(100);
+        if (!evaluationOuters.isEmpty()){
+            result.put("status",1);
+            evaluationOuters.remove("content");
+            result.put("data",evaluationOuters);
+        }
+        else {
+            result.put("status",0);
+            result.put("msg","获取失败");
+        }
+        return result;
+    }
     /*获取组间评价表内容*/
-    public Evaluation_outer showEvaluationOuter(int id){
-        Evaluation_outer evaluation_outer = group_evaluation.findByEvaluationOuterId(id);
-        return evaluation_outer;
+    public HashMap<String, Object> showEvaluationOuter(EvaluationOuter evaluationOuter){
+        HashMap<String,Object> result = new HashMap<>();
+        String flag = "0";
+        String msg = "获取失败，请检查评价表号";
+        EvaluationOuter getEvaluation = groupEvaluation.findByEvaluationOuterId(evaluationOuter.getEvaluationOuterId());
+        if (getEvaluation!=null){
+            result.put("status",1);
+            result.put("data",getEvaluation);
+        }
+        else {
+            result.put("status", 0);
+            result.put("msg", msg);
+        }
+        return result;
     }
 
     /*提交组间评价表*/
-    public boolean submitEvaluationOuter(int id,String name,String content,String time){
-        boolean flag = false;
-        Evaluation_outer evaluation_outer = new Evaluation_outer();
-        evaluation_outer.setContent(content);
-        evaluation_outer.setName(name);
-        evaluation_outer.setClass_id(id);
-        evaluation_outer.setRelease_time(time);
-        if (group_evaluation.save(evaluation_outer).getContent()!=null) flag=true;
-        return flag;
+    @Transactional
+    public HashMap<String, Object> submitEvaluationOuter(EvaluationOuter evaluationOuter){
+        int flag = 0;
+        String msg = "提交失败";
+        HashMap<String,Object> result = new HashMap<>();
+        EvaluationOuter saveEvaluation = new EvaluationOuter();
+        saveEvaluation.setContent(evaluationOuter.getContent());
+        saveEvaluation.setName(evaluationOuter.getName());
+        saveEvaluation.setClassId(evaluationOuter.getClassId());
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String time = formatter.format(date);
+        saveEvaluation.setReleaseTime(time);
+        if (groupEvaluation.save(saveEvaluation).getContent()!=null) {
+            flag = 1;
+            msg = "提交成功";
+        }
+        evaluationOuter.getContent().get("suggestions");
+        result.put("status",flag);
+        result.put("msg",msg);
+        return result;
     }
 }
