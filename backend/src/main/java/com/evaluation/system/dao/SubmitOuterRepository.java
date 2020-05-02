66203a4711531659c51c1f7b7add9e3a523132e0
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-02 21:53:06 +0800

    Update SubmitOuterRepository.java

--- backend/src/main/java/com/evaluation/system/dao/SubmitOuterRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/SubmitOuterRepository.java
@@ -1,8 +1,8 @@
 package com.evaluation.system.dao;
 
-import com.evaluation.system.bean.EvaluationInner;
 import com.evaluation.system.bean.SubmitOuter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubmitOuterRepository extends JpaRepository<SubmitOuter,Integer> {
+    public SubmitOuter findByEvaluationOuterIdAndGroupId(int evaluationId,int groupId);
 }
