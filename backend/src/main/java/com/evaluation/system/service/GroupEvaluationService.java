9c340b3404fabfff15f354dd6f6b555df03ffbbe
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-02 21:53:11 +0800

    Update GroupEvaluationService.java

--- backend/src/main/java/com/evaluation/system/service/GroupEvaluationService.java
+++ backend/src/main/java/com/evaluation/system/service/GroupEvaluationService.java
@@ -1,15 +1,18 @@
 package com.evaluation.system.service;
 
+
 import com.evaluation.system.bean.EvaluationOuter;
-import com.evaluation.system.dao.GroupEvaluationRepository;
+import com.evaluation.system.dao.EvaluationOuterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 221701109_肖玮昊
@@ -17,62 +20,27 @@ import java.util.List;
 @Service
 public class GroupEvaluationService {
     @Autowired
-    private GroupEvaluationRepository groupEvaluation;
+    private EvaluationOuterRepository evaluationOuterRepository;
 
     //按班级号显示组间评价列表
-    public  HashMap<String,Object> listEvaluation(EvaluationOuter evaluationOuter){
-        int classId = evaluationOuter.getClassId();
-        List<EvaluationOuter> evaluationOuters = groupEvaluation.findAllByClassId(classId);
-        HashMap<String,Object> result = new HashMap<>(100);
-        if (!evaluationOuters.isEmpty()){
-            result.put("status",1);
-            evaluationOuters.remove("content");
-            result.put("data",evaluationOuters);
+    @Transactional(rollbackFor = Exception.class)
+    public Map<String,Object> listEvaluation(EvaluationOuter evaluationOuter){
+        List<EvaluationOuter> evaluationOuters = evaluationOuterRepository.findAllByClassId(evaluationOuter.getClassId());
+        Map<String, Object> result = new HashMap<>();
+        try {
+            Map<String, Object> empty = new HashMap<>(2);
+            if (!evaluationOuters.isEmpty()) {
+                result.put("status", 1);
+                for (int i = 0; i < evaluationOuters.size(); i++) {
+                    evaluationOuters.get(i).setContent(empty);
+                }
+                result.put("data", evaluationOuters);
+            }
         }
-        else {
+        catch (Exception e){
             result.put("status",0);
             result.put("msg","获取失败");
         }
-        return result;
-    }
-    /*获取组间评价表内容*/
-    public HashMap<String, Object> showEvaluationOuter(EvaluationOuter evaluationOuter){
-        HashMap<String,Object> result = new HashMap<>();
-        String flag = "0";
-        String msg = "获取失败，请检查评价表号";
-        EvaluationOuter getEvaluation = groupEvaluation.findByEvaluationOuterId(evaluationOuter.getEvaluationOuterId());
-        if (getEvaluation!=null){
-            result.put("status",1);
-            result.put("data",getEvaluation);
-        }
-        else {
-            result.put("status", 0);
-            result.put("msg", msg);
-        }
-        return result;
-    }
-
-    /*提交组间评价表*/
-    @Transactional
-    public HashMap<String, Object> submitEvaluationOuter(EvaluationOuter evaluationOuter){
-        int flag = 0;
-        String msg = "提交失败";
-        HashMap<String,Object> result = new HashMap<>();
-        EvaluationOuter saveEvaluation = new EvaluationOuter();
-        saveEvaluation.setContent(evaluationOuter.getContent());
-        saveEvaluation.setName(evaluationOuter.getName());
-        saveEvaluation.setClassId(evaluationOuter.getClassId());
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String time = formatter.format(date);
-        saveEvaluation.setReleaseTime(time);
-        if (groupEvaluation.save(saveEvaluation).getContent()!=null) {
-            flag = 1;
-            msg = "提交成功";
-        }
-        evaluationOuter.getContent().get("suggestions");
-        result.put("status",flag);
-        result.put("msg",msg);
         return result;
     }
 }
