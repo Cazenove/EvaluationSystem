ea16692a7d1d1e5b4d090ae72d65ada6568eeed2
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-03 22:19:59 +0800

    修改Admin

--- backend/src/main/java/com/evaluation/system/bean/Admin.java
+++ backend/src/main/java/com/evaluation/system/bean/Admin.java
@@ -7,22 +7,20 @@ import javax.persistence.*;
 @Entity
 public class Admin{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private String admin_id;
-    @Column
+    private String adminId;
+
     private String password;
 
     public Admin(){
 
     }
 
-    public String getAdmin_id() {
-        return admin_id;
+    public String getAdminId() {
+        return adminId;
     }
 
-    public void setAdmin_id(String admin_id) {
-        this.admin_id = admin_id;
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
     }
 
     public String getPassword() {
