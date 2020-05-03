b7bf94f1099d787db381e985afa88b354eef67c4
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-03 22:08:08 +0800

    Update AdminRepository.java

--- backend/src/main/java/com/evaluation/system/dao/AdminRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/AdminRepository.java
@@ -3,5 +3,6 @@ package com.evaluation.system.dao;
 import com.evaluation.system.bean.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
+public interface AdminRepository extends JpaRepository<Admin,String> {
+    public Admin findByAdminId(String adminId);
 }
