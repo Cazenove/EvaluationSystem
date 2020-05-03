014df19caf00ef6085605fc4975a387739d7bb51
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-03 21:57:15 +0800

    Update EvaluationOuterRepository.java

--- backend/src/main/java/com/evaluation/system/dao/EvaluationOuterRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/EvaluationOuterRepository.java
@@ -1,8 +1,18 @@
 package com.evaluation.system.dao;
 
-import com.evaluation.system.bean.Admin;
 import com.evaluation.system.bean.EvaluationOuter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+/**
+ * @author acer
+ */
 public interface EvaluationOuterRepository extends JpaRepository<EvaluationOuter,Integer> {
+
+    //通过班级号查找
+    @Query("SELECT a FROM EvaluationOuter a WHERE a.classInfo.classId = :classId")
+    List<EvaluationOuter> findAllByClassId(@Param("classId")int classId);
 }
