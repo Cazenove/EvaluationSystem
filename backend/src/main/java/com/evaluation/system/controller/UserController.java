5b33473a7f6f7ad5ca3294abd945cf292846bb6d
xwh <60315356+xwh130@users.noreply.github.com>
2020-05-03 21:57:11 +0800

    Update UserController.java

--- backend/src/main/java/com/evaluation/system/controller/UserController.java
+++ backend/src/main/java/com/evaluation/system/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.evaluation.system.controller;
 
+import com.evaluation.system.bean.User;
 import com.evaluation.system.dao.UserRepository;
 import com.evaluation.system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +16,28 @@ public class UserController {
     private UserService userService;
 
     /*用户登录
-    *成功返回true
+    *@成功返回true
     * */
-    /*@PostMapping(value = "/login")
-    public boolean login(@RequestParam("user_id") String id,@RequestParam("password") String password){
-        return  userService.loginCheck(id,password);
-    }*/
+    @PostMapping(value = "/login")
+    public HashMap<String, Object> login(@RequestBody User user){
+        return  userService.loginCheck(user);
+    }
 
     /*用户注册
-    * 成功返回true
+    * @成功返回true
     * */
     @ResponseBody
     @PostMapping(value = "/register")
-    public HashMap<String, Object> register(@RequestParam("user_id") String id, @RequestParam("password") String password,
-                                            @RequestParam("name") String name, @RequestParam("class_id") int class_id,
-                                            @RequestParam("group_id") int group_id, @RequestParam("status") String status,
-                                            @RequestParam("telephone") String telephone){
-        return userService.registerCheck(id,password,name,class_id,group_id,status,telephone);
+    public HashMap<String, Object> register(@RequestBody User user){
+        return userService.registerCheck(user);
     }
 
     /*修改个人信息
     * 成功返回true
     * */
     @ResponseBody
-    @PostMapping(value = "/update/{user_id}")
-    public HashMap<String,Object> update(@PathVariable("user_id") String id, @RequestParam("password") String password,
-                          @RequestParam("name") String name, @RequestParam("telephone") String telephone){
-        return userService.updateCheck(id,password,name,telephone);
+    @PostMapping(value = "/user/update/{userId}")
+    public HashMap<String,Object> update(@RequestBody User user){
+        return userService.updateCheck(user);
     }
 }
