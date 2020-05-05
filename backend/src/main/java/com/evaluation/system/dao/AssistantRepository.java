313ed3c3f186c8add90e796f3a2cd8710854360f
13215007537 <616854208@qq.com>
2020-05-05 23:43:03 +0800

    接口13到20所需的函数

--- backend/src/main/java/com/evaluation/system/dao/AssistantRepository.java
+++ backend/src/main/java/com/evaluation/system/dao/AssistantRepository.java
@@ -2,7 +2,17 @@ package com.evaluation.system.dao;
 
 import com.evaluation.system.bean.Assistant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssistantRepository extends JpaRepository<Assistant,String> {
     public Assistant findByAssistantId(String id);
+    //助教id查询
+    @Query("select a from Assistant as a where a.assistant_id = :assistant_id")
+    Assistant findByAssistantId(@Param("assistant_id") String assistant_id);
+    //助教电话查询
+    @Query("select a from Assistant as a where a.telephone = :telephone")
+    Assistant findByTelephone(@Param("telephone") String telephone);
 }
