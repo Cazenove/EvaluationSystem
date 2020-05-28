99ac8b43ae300ad0da2fc03dec5174000b0367df
Etoilehey <895623136@qq.com>
2020-05-28 22:25:01 +0800

    - 后台首页修改--
    -  修改

--- frontend/evaluation/src/views/admin/AdminHome.vue
+++ frontend/evaluation/src/views/admin/AdminHome.vue
@@ -1,16 +1,31 @@
 <template>
 	<div>
 		<ManageNav />
-		<div class="container">
-			<div class="form-group">
+		<div class="container col-md-10">
+			<div class="form-group col-md-3">
 				<label for="classId" class="col-form-label">班级</label>
 				<select class="form-control" v-model="classId" @change="getClassInfo()">
 					<option disabled="disabled" :value="null">请选择</option>
 					<option v-for="(item, index) in classList" :value="item.classId" :key="item.classId">{{item.className}}</option>
 				</select>
 			</div>
-			
-			<div id="myChart" :style="{width: '500px', height: '500px'}"></div>
+			<div class="row">
+				<div id="myChart" class="card col-md-7" :style="{height: '500px'}"></div>
+				<div class="card col-md-3" :style="{height: '500px'}">
+					<h5>小组综合得分</h5>
+					<div class="list-group">
+						<button type="button" class="btn btn-success list-group-item list-group-item-action">
+							<span class="badge badge-info">1</span>第1组 85
+						</button>
+						<button type="button" class="btn btn-success list-group-item list-group-item-action">
+							<span class="badge badge-info">2</span>第2组 84
+						</button>
+						<button type="button" class="btn btn-success list-group-item list-group-item-action">
+							<span class="badge badge-info">3</span>第3组 83
+						</button>
+					</div>
+				</div>
+			</div>
 			<!-- <Ranjintu /> -->
 		</div>
 	</div>
@@ -33,8 +48,7 @@
 			return {
 				classList: [],
 				classId: '',
-				classInfo: {
-				},
+				classInfo: {},
 				option: {
 					title: {
 						text: '小组得分折线图'
@@ -46,15 +60,9 @@
 						data: []
 					},
 					grid: {
-						left: '3%',
-						right: '4%',
-						bottom: '3%',
+						right: '30%',
+						bottom: '40%',
 						containLabel: true
-					},
-					toolbox: {
-						feature: {
-							saveAsImage: {}
-						}
 					},
 					xAxis: {
 						type: 'category',
@@ -74,21 +82,21 @@
 		methods: {
 			getClassList() {
 				var self = this;
-				axios.get(api.adminClassList,null)
-				.then(function(res) {
-					self.classList = res.data.data;
-				}).catch(function(error) {
-					console.log(error);
-				})
+				axios.get(api.adminClassList, null)
+					.then(function(res) {
+						self.classList = res.data.data;
+					}).catch(function(error) {
+						console.log(error);
+					})
 			},
 			getClassInfo() {
 				var self = this;
 				this.init();
-				axios.post(api.userEvaluationOuterList,{
-					classId:this.classId
+				axios.post(api.userEvaluationOuterList, {
+					classId: this.classId
 				}).then(function(res) {
 					if (res.status == 200 && res.data.status == 1) {
-						for(var i=0; i<res.data.data.length; i++) {
+						for (var i = 0; i < res.data.data.length; i++) {
 							self.option.xAxis.data[i] = res.data.data[i].name
 						}
 						self.drawLine();
@@ -96,33 +104,33 @@
 				}).catch(function(error) {
 					console.log(error);
 				})
-				axios.get(api.adminGroupScoreList,null)
-				.then(function(res) {
-					if (res.status == 200 && res.data.status == 1) {
-						var data = {};
-						for(var i=0; i<res.data.data.length; i++) {
-							if(res.data.data[i].classId == self.classId) {
-								if(!data[res.data.data[i].groupId]) {
-									data[res.data.data[i].groupId] = {
-										name: res.data.data[i].groupName,
-										type: 'line',
-										data: []
-									};
-									data[res.data.data[i].groupId].data.push(res.data.data[i].content);
-								} else {
-									data[res.data.data[i].groupId].data.push(res.data.data[i].content);
+				axios.get(api.adminGroupScoreList, null)
+					.then(function(res) {
+						if (res.status == 200 && res.data.status == 1) {
+							var data = {};
+							for (var i = 0; i < res.data.data.length; i++) {
+								if (res.data.data[i].classId == self.classId) {
+									if (!data[res.data.data[i].groupId]) {
+										data[res.data.data[i].groupId] = {
+											name: res.data.data[i].groupName,
+											type: 'line',
+											data: []
+										};
+										data[res.data.data[i].groupId].data.push(res.data.data[i].content);
+									} else {
+										data[res.data.data[i].groupId].data.push(res.data.data[i].content);
+									}
 								}
 							}
+							var j = 0;
+							for (var i in data) {
+								self.option.series[j++] = data[i];
+							}
+							console.log(self.option);
 						}
-						var j = 0;
-						for(var i in data) {
-							self.option.series[j++] = data[i];
-						}
-						console.log(self.option);
-					}
-				}).catch(function(error) {
-					console.log(error);
-				})
+					}).catch(function(error) {
+						console.log(error);
+					})
 			},
 			init() {
 				this.option = {
@@ -141,11 +149,6 @@
 						bottom: '3%',
 						containLabel: true
 					},
-					toolbox: {
-						feature: {
-							saveAsImage: {}
-						}
-					},
 					xAxis: {
 						type: 'category',
 						boundaryGap: false,
@@ -162,6 +165,9 @@
 				let myChart = this.$echarts.init(document.getElementById('myChart'))
 				// 绘制图表
 				myChart.setOption(this.option);
+				window.onresize = function() {
+					myChart.resize();
+				};
 			}
 		}
 	}
